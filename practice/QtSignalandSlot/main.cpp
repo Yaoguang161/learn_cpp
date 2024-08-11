5426a90e3ff9b067a7ba3c9f2be7e422c1ef7f46
yaoguang161 <yaoguang161@outlook.com>
2024-08-11 23:00:46 +0800

    实现Qt信号槽

--- practice/QtSignalandSlot/main.cpp
+++ practice/QtSignalandSlot/main.cpp
@@ -1,47 +1,22 @@
 ﻿#include <iostream>
-#include <functional>
-#include <vector>
-
-//... 在左边 define
-//... 右边   use
-
-template <class Self, class MemFn>
-auto bind(Self* self, MemFn memfn) {
-    return[self, memfn](auto ...t) {  //...在左边,属于定义形参包
-        (self->*memfn)(t...);         //成员函数指针用法
-        };
-}
-
-template <class ...T>  //...在左边表示定义一个形参包  使用大写T
-struct Signal {
-    std::vector<std::function<void(T...)>> m_callbacks; //...在右边表示使用一个形参包 使用大写T
-
-    void connect(std::function<void(T...)> callback) {
-        m_callbacks.push_back(std::move(callback));
-    }
-
-    template <class Self, class MemFn>
-    void connect(std::function<void(T...)> callback) {
-        m_callbacks.push_back(std::move(callback));
-    }
-
-    void emit(T... t) {  //使用大写T,  定义小写t
-        for (auto&& callback : m_callbacks) {
-            callback(t...);
-        }
-    }
-};
+#include <memory>
+#include <string>
+#include "Signal.hpp"
 
 struct Foo {
-    void on_input(int i) {
-        std::cout << "Foo of age " << age << " got " << i << '\n';
+    void on_input(int i, int j) const {
+        std::cout << "Foo of age " << age << " got i=" << i << ", j=" << j << '\n';
     }
 
     int age = 14;
+
+    ~Foo() {
+        std::cout << "Foo destruct\n";
+    }
 };
 
 struct Bar {
-    void on_input(int i) {
+    void on_input(int i) const {
         std::cout << "Bar of age " << age << " got " << i << '\n';
     }
 
@@ -49,73 +24,67 @@ struct Bar {
         std::cout << "Bar got exit event: " << msg1 << " " << msg2 << '\n';
     }
 
-
     int age = 42;
 };
 
+std::shared_ptr<Bar> gbar;
+std::shared_ptr<void> gmine;
+
 struct Input {
-    void main_loog() {
+    void main_loop() {
         int i;
         while (std::cin >> i) {
             on_input.emit(i);
+            if (i == 42)
+                gbar = nullptr;
         }
         on_exit.emit("hello", "world");
     }
 
-    Signal<int> on_input;
-    Signal<std::string, std::string> on_exit;
+    MySig::Signal<int> on_input;
+    MySig::Signal<std::string, std::string> on_exit;
 };
 
+void test(std::string msg1, std::string msg2) {
+    std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
+}
 
-void (Bar::* memfn) (std::string, std::string);
-void (*putfn)(std::string, std::string);
+void test(int msg1, std::string msg2) {
+    std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
+}
 
-//#define FUN(fun) [&] (auto ...t) {return fun(t...);}
-//万能引用版本
-#define FUN(__fun) [&] (auto &&...__t) {return __fun(std::forward<decltype(__t)>(__t)...);}
-
-////方法一
-//void test(std::string msg1, std::string msg2) {
-//    std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
-//}
-//
-//void test(int msg1, std::string msg2) {
-//    std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
-//}
-
-//方法二
-struct Test {
-    void operator() (std::string msg1, std::string msg2) {
-        std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
+struct Mine : std::enable_shared_from_this<Mine> { // Mine 必须从 make_shared 创建
+    void register_on(Input& input) {
+        input.on_input.connect(weak_from_this(), &Mine::on_input);
     }
 
-    void operator() (int msg1, std::string msg2) {
-        std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
+    void on_input(int i) {
+        std::cout << "Mine got i=" << i << '\n';
+        if (i == 5) {
+            gmine = nullptr;
+        }
     }
-} test;  //定义成对象
 
-int main()
-{
+    ~Mine() {
+        std::cout << "Mine destruct\n";
+    }
+};
+
+void dummy(Input& input) {
+    auto bar = std::make_shared<Bar>();
+    input.on_input.connect(std::weak_ptr<Bar>(bar), &Bar::on_input, MySig::nshot_t(10)); // 调用 10 次后回调自动无效化
+    gbar = bar;
+    auto mine = std::make_shared<Mine>();
+    mine->register_on(input);
+    gmine = mine;
+}
+
+int main() {
     Input input;
-    Bar bar;
-    Foo foo;
-
-    input.on_input.connect([&](auto ...t) {foo.on_input(t...); });
-    //input.on_input.connect(foo.on_input); 这样不允许, 但是下面的套皮就可以
-    input.on_input.connect(FUN(foo.on_input));
-    input.on_input.connect(bind(&foo, &Foo::on_input));
-    input.on_input.connect(bind(&bar, &Bar::on_input));
-    input.on_exit.connect(bind(&bar, &Bar::on_exit));
-
-    //定义一个全局函数,如果直接调用,无法判断是哪个,但是套皮就可以
-    //input.on_exit.connect(test);  //如果使用struct 定义成对象就可以 见方法二
-    //input.on_exit.connect(static_cast<void(*)(std::string, std::string)>(test));  //方法三
-    input.on_exit.connect(FUN(test));  //套皮法,最简单   方法一
-
-
-    input.on_exit.connect([&](std::string msg1, std::string msg2) {
-        std::cout << "main received exit event: " << msg1 << " " << msg2 << '\n';
+    dummy(input);
+    input.on_input.connect([=](int i) {
+        std::cout << "main received input: " << i << '\n';
         });
-
-    input.main_loog();   //循环输入
+    input.main_loop();
+    return 0;
 }
